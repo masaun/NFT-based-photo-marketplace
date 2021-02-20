da656dfca012914967aa2174bfd8158bafbe7484
masaun <masanoriuno@gmail.com>
2021-02-20 21:27:23 +0900

    Remove the PhotoNFTFactoy from migration file of PhotoNFTMarketPlace.sol

--- migrations/3_deploy_PhotoNFTMarketPlace.js
+++
@@ -1,8 +0,0 @@
-const PhotoNFTMarketPlace = artifacts.require("./PhotoNFTMarketPlace.sol");
-const PhotoNFTFactory = artifacts.require("./PhotoNFTFactory.sol");
-
-const _photoNFTFactory = PhotoNFTFactory.address;
-
-module.exports = async function(deployer, network, accounts) {
-    await deployer.deploy(PhotoNFTMarketPlace, _photoNFTFactory);
-};
